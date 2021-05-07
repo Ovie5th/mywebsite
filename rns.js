8c697d56cdbbae34fd2b9528f72263450e360759
ovie idholo <ovie5th@gmail.com>
2021-05-07 08:00:31 +0100

    third commit
    third commit

--- rns.js
+++ rns.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const togglebutton = document.querySelector(".toggle_button");
-const mobileNav = document.querySelector(".mobile_nav_list");
+const mobileNav = document.querySelector(".mobile_nav_wrapper");
 hamburger.addEventListener('click', () => {
     mobileNav.classList.toggle("open");
 });
